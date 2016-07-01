ea9111c4e664a7b8a13da30f071ca4c2810afdbe
Sai Karthik Reddy Ginni <sg3205@columbia.edu>
2016-06-30 23:44:47 -0400

    Added promise-loader to do code-splitting for different routes

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,6 +1,15 @@
-import React from 'react'
+/* eslint no-unused-vars: 0  */
+/* eslint import/no-unresolved: [2, { ignore: ['^promise\?global.*'] }]*/
+import React                 from 'react'
 import { Route, IndexRoute } from 'react-router'
-import App from './components/App'
+
+// Import the component normally in case you want to bundle with the starting javascript file
+import App                   from './components/App'
+
+// Lazy Load the Paths (Do this to do code-splitting so that the pages javascript chunk) only gets
+// loaded if the url is accessed
+import Home                  from 'promise?global!./components/Home'
+import About                 from 'promise?global!./components/About'
 
 function handleError(err) {
 	// TODO : Error Handling
@@ -8,31 +17,57 @@ function handleError(err) {
 	console.log(err) // eslint-disable-line no-console
 }
 
-function resolveIndexComponent(nextState, cb) {
-	System.import('./components/Home')
-		.then(module => cb(null, module.default))
-		.catch(handleError)
+function lazyLoadComponent(lazyModule) {
+	return (nextState, cb) =>
+		lazyModule()
+			.then(module => cb(null, module.default))
+			.catch(handleError)
 }
 
-function resolveAboutComponent(nextState, cb) {
-	System.import('./components/About')
-		.then(module => cb(null, module.default))
-		.catch(handleError)
+function lazyLoadComponents(lazyModules) {
+	return (nextState, cb) => {
+		const moduleKeys = Object.keys(lazyModules)
+		const promises = moduleKeys.map(key => lazyModules[key])
+		Promise.all(promises)
+			.then(modules => {
+				cb(null, modules.reduce((acc, module, i) =>
+					// Essentially we are doing acc[moduleKeys[i]] = module; return acc;
+					// We are doing this way to aviod mutation
+					// In the new ES2015 Standard, dynamic keys can be created using then following
+					// syntax
+					//
+					//	let obj = {
+					//	  [myKey]: value
+					//	}
+					//
+					// This is similar to doing
+					//
+					// let temp = myKey /* Here myKey is a dynamically computed value like moduleKeys[i] */
+					// let obj = {
+					//	  temp: value
+					// }
+					//
+					Object.assign({}, acc, {
+						[moduleKeys[i]]: module.default
+					}), {}))
+			})
+			.catch(handleError)
+	}
 }
 
 /**
  * Our routes.
  *
  * NOTE: We load our routes asynhronously using the `getComponent` API of
- * react-router, doing so combined with the `System.import` support by
- * webpack 2 allows us to get code splitting based on our routes.
+ * react-router, doing so combined with the `System.import` support by webpack 2
+ * or in our case using `promise-loader` allows us to get code splitting based on our routes.
  * @see https://github.com/reactjs/react-router/blob/master/docs/guides/DynamicRouting.md
  * @see https://gist.github.com/sokra/27b24881210b56bbaff7#code-splitting-with-es6
  */
 const routes = (
 	<Route path="/" component={App}>
-		<IndexRoute getComponent={resolveIndexComponent} />
-		<Route path="/about" getComponent={resolveAboutComponent} />
+		<IndexRoute getComponent={lazyLoadComponent(Home)} />
+		<Route path="/about" getComponent={lazyLoadComponent(About)} />
 	</Route>
 )
 
